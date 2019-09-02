5bd31466e5298edb7e9a7dfe0e469350331c33f1
Francisco J. Piedrahita <fpiedrah93@gmail.com>
2019-09-01 19:37:35 -0500

    Solved forma issues un setup.py

--- packages/python/setup.py
+++ packages/python/setup.py
@@ -1,28 +1,35 @@
 from distutils.core import setup
 
 
-with open('VERSION', 'r') as f:
+with open("VERSION", "r") as f:
     version = f.read()
 
-with open('README.md','r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r') as f:
-    install_requires = f.read().split('\n')
+with open("./requirements/prod.txt", "r") as f:
+    install_requires = f.read().split("\n")
 
 setup(
-    name='SPL',
+    name="SPL",
     version=version,
-    author='Franciscp J. Piedrahita',
-    author_email='fpiedrah93@gmail.com',
-
-    packages=['spl', 'tests'],
-    url='https://github.com/SPLA/SPL-Solver',
-
-    license='MIT',
-    description='Simple library to write decision trees',
+    author="Franciscp J. Piedrahita",
+    author_email="fpiedrah93@gmail.com",
+    packages=["spl", "tests"],
+    url="https://github.com/SPLA/SPL-Solver",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Product Lines :: Libraries :: Python Modules",
+        "Topic :: Internet",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    description="Utilities for software product lines",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
+    python_requires=">=3",
     install_requires=install_requires,
 )
